07af127ecf2193232b802f01b6ae919560b7bd96
Deepak kumar <deepak7589467031@gmail.com>
2024-06-10 21:59:19 +0530

    first commit

---
+++ backend/db/connectMongoDB.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const connectMongoDB = async () => {
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI);
+		console.log(`MongoDB connected: ${conn.connection.host}`);
+	} catch (error) {
+		console.error(`Error connection to mongoDB: ${error.message}`);
+		return;
+	}
+};
+
+export default connectMongoDB;
